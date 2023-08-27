5a8d5a3842b68f88cb97334f7dec10bf8d21ee9d
Michael Epper <michael.epper@ksb-sg.ch>
2023-08-27 23:04:05 +0200

    Hausaufgabe

---
+++ OO_functions/MyMath.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OO_functions
+{
+    public static class MyMath
+    {
+        public static int Calc_ggT(int a, int b)
+        {
+            int z = a;
+            while(b != 0)
+            {
+                z = a % b;
+                a = b;
+                b = z;
+            }
+            return a;
+        }
+
+        public static int Calc_kgV(int a, int b, int ggT) 
+        { 
+            int z = a * b / ggT;
+            return z;
+        }
+
+        public static int ReadInt()
+        {
+            int zahl;
+            do
+            {
+                Console.WriteLine("Bitte geben Sie eine positive Ganzzahl ein: ");
+                zahl = Convert.ToInt32(Console.ReadLine());
+
+            } while (zahl < 1);
+
+            return zahl;
+        }
+
+        public static int ShowResult(string arit, int a, int b, int zahl)
+        {
+            Console.WriteLine(arit + " von " + a + " und " + b + " ist " + zahl);
+            return 0;
+        }
+
+        public static int[] ReadArray()
+        {
+            Console.WriteLine("Wie viele Zahlen soll Ihr Array haben?");
+            int laenge = MyMath.ReadInt();
+            int[] array = new int[laenge];
+            for (int i = 0; i < laenge; i++)
+            {
+                array[i] = MyMath.ReadInt();
+            }
+            return array;
+        }
+        public static int Calc_ggT_r(int a, int b)
+        {
+            if (b == 0)
+            {
+                return a;
+            }
+            else
+            {
+                return Calc_ggT_r(b, a % b);
+            }
+        }
+
+
+        public static void ShowArray(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i]);
+
+                if (i < array.Length - 1)
+                {
+                    Console.Write(", "); 
+                }
+            }
+            Console.WriteLine();
+        }
+
+        public static int AritMittelwert(int[] array)
+        {
+            int summe = 0;
+            foreach(int zahl in array)
+            {
+                summe += zahl;
+            }
+            return summe / array.Length;
+        }
+
+        public static int KleinsterWert(int[] array)
+        {
+            int kleinsteZahl = array[0];
+            foreach (int zahl in array)
+            {
+                if(zahl < kleinsteZahl)
+                {
+                    kleinsteZahl = zahl;
+                }
+            }
+            return kleinsteZahl;
+        }
+
+        public static int GroessterWert(int[] array)
+        {
+            int groessteZahl = 0;
+            foreach (int zahl in array)
+            {
+                if (zahl > groessteZahl)
+                {
+                    groessteZahl = zahl;
+                }
+            }
+            return groessteZahl;
+        }
+
+        public static void Swap(ref int a, ref int b)
+        {
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+
+        public static int[] SwapArray(int[] array)
+        {
+            int[] newArray = new int[array.Length];
+            for (int i = 0; i < array.Length; i++)
+            {
+                newArray[array.Length - 1 - i] = array[i];
+            }
+            return newArray;
+        }
+
+    }
+}
--- OO_functions/Program.cs
+++ OO_functions/Program.cs
@@ -1,10 +1,63 @@
-﻿namespace OO_functions
+﻿using System.Dynamic;
+
+namespace OO_functions
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello, World!");
+            // 1. ggT und kgV berechnen
+            Console.WriteLine("1. ggT und kgV berechnen\n");
+            int a = MyMath.ReadInt();
+            int b = MyMath.ReadInt();
+            int ggT = MyMath.Calc_ggT(a, b);
+            int kgV = MyMath.Calc_kgV(a, b, ggT);
+            MyMath.ShowResult("ggT", a, b, ggT);
+            MyMath.ShowResult("kgV", a, b, kgV);
+
+
+            // 2. ggT rekursiv berechnen
+            Console.WriteLine("\n2. ggT rekursiv berechnen");
+            ggT = MyMath.Calc_ggT_r(a, b);
+            MyMath.ShowResult("ggT", a, b, ggT);
+
+
+            // 3. arithmetischer Mittelwert, kleinster Wert und grösster Wert eines Arrays berechnen
+            Console.WriteLine("\n3. arithmetischer Mittelwert, kleinster Wert und grösster Wert eines Arrays berechnen\n");
+            int[] array = MyMath.ReadArray();
+            int aritMittelwert = MyMath.AritMittelwert(array);
+            Console.WriteLine("Arithmetischer Mittelwert: " + aritMittelwert);
+
+            int klWert = MyMath.KleinsterWert(array);
+            Console.WriteLine("Kleinster Wert: " + klWert);
+
+            int grWert = MyMath.GroessterWert(array);
+            Console.WriteLine("Groesster Wert: " + grWert);
+
+
+            // 4. Inhalt von zwei übergebenen Variablen tauschen
+            Console.WriteLine("\n4. Inhalt von zwei übergebenen Variablen tauschen\n");
+            a = MyMath.ReadInt();
+            b = MyMath.ReadInt();
+            Console.WriteLine("Vor der Vertauschung: a = {" + a + "}, b = {" + b + "}");
+            MyMath.Swap(ref a, ref b);
+            Console.WriteLine("Nach der Vertauschung: a = {" + a + "}, b = {" + b + "}");
+
+
+            // 5. Array umkehren
+            Console.WriteLine("\n5. Array umkehren");
+            array = MyMath.ReadArray();
+            Console.WriteLine("Array vor Umkehrung: ");
+            MyMath.ShowArray(array);
+            array = MyMath.SwapArray(array);
+            Console.WriteLine("Array nach Umkehrung: ");
+            MyMath.ShowArray(array);
+
+
+            // Main Funktion erneut aufrufen
+            Console.ReadKey();
+            Console.Clear();
+            Main();
         }
     }
 }
